fb32a97a31396135e548bb8fa48344e0aab449b9
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-05 16:20:56 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:20:48.512Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T13:20:56.441Z
+Commit: Fix memory leak in event handlers
