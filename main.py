a7f5c5af2bc08908a62679229e8fe03e439e47ea
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-05 16:20:46 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:20:14.793Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T13:20:45.481Z
+Commit: Fix memory leak in event handlers
