f990428d604dd3c0d2bae6789c8f9840eda2e702
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-05 16:19:56 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:19:31.079Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T13:19:55.765Z
+Commit: Resolve null pointer exception in user service
