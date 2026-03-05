c2ba47cf4830f4ce20e1493fd57b2ad6310520d9
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-05 16:20:00 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:19:55.765Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T13:20:00.425Z
+Commit: Fix CSS styling bug on mobile devices
