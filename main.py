38a1b0a7b42cd2935fbd0d7a97809c7371007bf3
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-05 16:20:13 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:20:00.425Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T13:20:13.297Z
+Commit: Resolve null pointer exception in user service
