5b74f113ba3d6906585efd5928076f6b58fc554c
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-05 16:20:40 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:20:28.405Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T13:20:39.538Z
+Commit: Fix memory leak in event handlers
