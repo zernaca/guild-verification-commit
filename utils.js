82aa6e9d22a2700243679fa9847a4183d9fe5714
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-05 16:27:46 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:27:40.802Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T13:27:45.559Z
+Commit: Resolve null pointer exception in user service
