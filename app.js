975063b16bda8e719ba3fdf707134efed899c1ec
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-27 15:49:58 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T12:49:42.649Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-27T12:49:57.989Z
+Commit: Fix broken links in documentation
