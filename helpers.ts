30673a16955e23bdda7db5b4c7eb7faa7e0123db
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-03-27 15:49:57 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T12:49:53.012Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-27T12:49:56.748Z
+Commit: Fix memory leak in event handlers
