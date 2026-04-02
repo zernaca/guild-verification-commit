02a69ba2e10b6a5adfa8b2503316043baa4ccaa8
NacaZerne <93059016+zernaca@users.noreply.github.com>
2026-04-02 17:15:54 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-02T14:15:51.154Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-02T14:15:54.120Z
+Commit: Fix broken links in documentation
